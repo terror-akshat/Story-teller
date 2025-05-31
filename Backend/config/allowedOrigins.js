accc56c4b8cd0dc5a3b59f266da591cc1e87050c
terror-akshat <www.akshatagarwal123klj@gmail.com>
2025-05-31 16:18:15 +0530

    update

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   // "https://imagine-tellmeastory-git-dev-trace-kadenyi.vercel.app/",
   "https://imagine-tellmeastory.vercel.app",
+  "https://beamish-chimera-f93fd5.netlify.app/login",
   "http://localhost:3001",
   "http://localhost:4000",
 ];
